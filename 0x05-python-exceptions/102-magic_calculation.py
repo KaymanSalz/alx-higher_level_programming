4d2051a847885b15080923cd87ec720446996991
Kayman <35963738+KaymanSalz@users.noreply.github.com>
2022-09-13 14:16:22 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def raise_exception_msg(message=""):
-    raise NameError(message)
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too Far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return result
