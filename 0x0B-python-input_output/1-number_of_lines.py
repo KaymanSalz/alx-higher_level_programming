7a4fd0560bf344da2049e293fa5cd16b5cca825b
Kayman <35963738+KaymanSalz@users.noreply.github.com>
2022-09-28 16:19:26 +0100

    Update 1-number_of_lines.py

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-"""Module 1-number_of_lines.
-Counts number of lines in a file.
-"""
+"""Provides a function to count the lines in a file"""
 
 
 def number_of_lines(filename=""):
-    """Counts lines in filename.
-
-    Args:
-        - filename: name of the file
-
-    Returns:
-        - number of lines
-    """
-
-    count = 0
-
-    with open(filename) as f:
-        text = f.readlines()
-        for line in text:
-            count += 1
-
-    return count
+    """Count the number of lines in a file"""
+    with open(filename, 'r') as istream:
+        return len(istream.readlines())
