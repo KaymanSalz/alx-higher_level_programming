bd866f7703a0d85b44711e066154bfd86a57ea8f
Kayman <35963738+KaymanSalz@users.noreply.github.com>
2022-11-03 14:43:32 +0100

    temperatures

--- 0x0D-SQL_introduction/temperatures.sql
+++ 0x0D-SQL_introduction/temperatures.sql
@@ -29,7 +29,7 @@ CREATE TABLE `temperatures` (
   `month` int(11) DEFAULT NULL,
   `value` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET character_set_client = @saved_cs_client 1*/;
 
 --
 -- Dumping data for table `temperatures`
